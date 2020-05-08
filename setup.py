6a4991455222766d397c7fa37ce55081e4ae82e2
marygeorgiou <georgiou.mv@gmail.com>
2020-05-08 12:33:26 +0200

    pump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 setup(
       name='bioCaption',  # Required
-      version='1.0',
+      version='1.0.2',
       description='Diagnostic Captioning',
       url='https://github.com/nlpaueb/dc',
       author='nlpaueb',
@@ -21,5 +21,5 @@ setup(
       keywords='diagnostic image captioning machine learning',
       packages=find_packages(),
       python_requires='>=3.6',
-      install_requires=['bs4', 'pandas', 'img2vec_pytorch', 'gensim', 'Cython', 'pycocoevalcap'],
+      install_requires=['bs4'],
 )
